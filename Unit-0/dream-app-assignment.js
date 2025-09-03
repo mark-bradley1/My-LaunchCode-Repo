6c1f5b15a6e113ff9974ea95e992393a40badf79
Mark Bradley <mbradle15@gmail.com>
2025-09-03 12:19:56 -0500

    updated

--- Unit-0/dream-app-assignment.js
+++ Unit-0/dream-app-assignment.js
@@ -16,7 +16,8 @@ let store2 = [
     ['pickels', 4.49],
 ]; // Building Arrays; Values, Data Types and Operations
 
-let itemName = "milk"; // Values, Data Types and Operations
+let itemName = readline.question("Please enter item to search for (milk, eggs, chips, bread, pickels): ")
+let userItem = itemName; // Values, Data Types and Operations
 let shoppingCart = []; // Building Arrays
 
 let price1 = null; // Values, Data Types and Operations
@@ -24,25 +25,25 @@ let price2 = null; // Values, Data Types and Operations
 
 console.log("Searching Store 1:"); 
 for (let i = 0; i < store1.length; i++) { // Stringing Characters Together
-  if (store1[i][0] === itemName) {
+  if (store1[i][0] === userItem) {
     price1 = store1[i][1];
-    console.log(`Store 1 - ${itemName}: $${store1[i][1].toFixed(2)}`); // Control Structures and Logic; Stringing Characters Together; Using Arrays
+    console.log(`Store 1 - ${userItem}: $${store1[i][1].toFixed(2)}`); // Control Structures and Logic; Stringing Characters Together; Using Arrays
   } // Control Structures and Logic; Using Arrays
 } // Working with Loops, Using Arrays
 
 console.log("Searching Store 2:");
 for (let i = 0; i < store2.length; i++) { //Stringing Characters Together
-  if (store2[i][0] === itemName) {
+  if (store2[i][0] === userItem) {
     price2 = store2[i][1];
-    console.log(`Store 2 - ${itemName}: $${store2[i][1].toFixed(2)}`); // Control Structures and Logic; Stringing Characters Together; Using Arrays
+    console.log(`Store 2 - ${userItem}: $${store2[i][1].toFixed(2)}`); // Control Structures and Logic; Stringing Characters Together; Using Arrays
   } // Control Structures and Logic; Using Arrays
 } // Working with Loops; Using Arrays
 
 if (price1 !== null && price2 !== null) {
     if (price1 < price2) {
-        shoppingCart.push([itemName, price1, 'store1']); // Control Structures and Logic; Using Arrays
+        shoppingCart.push([userItem, price1, 'store1']); // Control Structures and Logic; Using Arrays
     } else {
-        shoppingCart.push([itemName, price2, 'store2']); // Control Structures and Logic; Using Arrays
+        shoppingCart.push([userItem, price2, 'store2']); // Control Structures and Logic; Using Arrays
     }
 } else {
     console.log('Item not found in one of the stores.'); // Control Structures and Logic
